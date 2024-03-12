399ac7f1fcf70227e2b54478a97b16e78385239c
ssaranya27 <ssaranya27072005@gmail.com>
2024-03-12 11:47:34 +0530

    success

--- sara.py
+++ sara.py
@@ -1,27 +1,4 @@
-# EX01 Developing a Simple Webserver
-## Date: 14/02/2024
-
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-```from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <html>
 <title align="center"> TOP SOFTWARE COMPANIES WITH REVENUE </title>
@@ -84,15 +61,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-```
-
-
-
-## OUTPUT:
-![alt text](<simple webserver.png>)
-
-![alt text](terminal.png)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
